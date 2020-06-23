8812c5d0ad00890918f263fe351fb62acc857e63
Ebrahim Ghanbari <ghanbari@ualberta.ca>
2020-06-23 01:17:10 +0000

    join btw tables

---
+++ 2_queries_joins/1_cohort_total_assigment_duration.sql
@@ -0,0 +1,4 @@
+SELECT  SUM(duration) 
+FROM assignment_submissions INNER JOIN students ON students.id = student_id
+INNER JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12'; 
